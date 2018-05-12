2ca75b3acb94474079e25911a257ed8f50bdc05b
Sebastian Fuhrmann <info@sebastianfuhrmann.de>
2018-05-12 14:16:05 +0200

    change order of config requests; add error output

--- speedtest.php
+++ speedtest.php
@@ -9,34 +9,41 @@
 			public function test(){
 				$result = array();
 
-				// getting active servers
-				$this->logtxt('Getting active speedtest servers...');
-				$servers = $this->getServers();
-				if (!$servers){
-					$this->logtxt('Error fetching server list!', 'error');
+				// getting configuration
+				$this->logtxt('Getting speedtest configuration...');
+				$result['config'] = $this->getConfig();
+
+				if (!$result['config']){
+					$this->logtxt('Error fetching speedtest configuration!', 'error');
 				} else {
-					// getting configuration
-					$this->logtxt('Getting speedtest configuration...');
-					$result['config'] = $this->getConfig();
+					// getting active servers
+					$this->logtxt('Getting active speedtest servers...');
+					$servers = $this->getServers();
+					if (!$servers){
+						$this->logtxt('Error fetching server list!', 'error');
+					} else {
+						// search nearest server
+						$this->logtxt('Calculating nearest server based on its position...');
+						$result['server'] = $this->getNearestServer($servers, array('lat' => $result['config']['client']['lat'], 'lon' => $result['config']['client']['lon']));
 
-					// search nearest server
-					$this->logtxt('Calculating nearest Server based on position...');
-					$result['server'] = $this->getNearestServer($servers, array('lat' => $result['config']['client']['lat'], 'lon' => $result['config']['client']['lon']));
-
-					// latency
-					$latencies = array();
-					for ($i = 0; $i < 3; $i++){
-						$url = $result['server']['url'].'/latency.txt?x='.(microtime(true)*1000).'.'.$i;
-						$info = $this->getHTTPInfo($url);
-						if ($info && $info["connect_time"]){
-							$latencies[] = $info["connect_time"];
+						if (!$result['server']){
+							$this->logtxt('Error calculating nearest server!', 'error');
 						} else {
-							$i -= 1;
+							// latency
+							$latencies = array();
+							for ($i = 0; $i < 3; $i++){
+								$url = $result['server']['url'].'/latency.txt?x='.(microtime(true)*1000).'.'.$i;
+								$info = $this->getHTTPInfo($url);
+								if ($info && $info["connect_time"]){
+									$latencies[] = $info["connect_time"];
+								} else {
+									$i -= 1;
+								}
+							}
+							$result['latency'] = round((array_sum($latencies)/count($latencies))*1000, 2);
+							$this->logtxt('Latency: '.$result['latency'].' ms');
 						}
 					}
-					$result['latency'] = round((array_sum($latencies)/count($latencies))*1000, 2);
-					$this->logtxt('Latency: '.$result['latency'].' ms');
-					
 				}
 
 				return $result;
@@ -55,7 +62,6 @@
 				$c = 2 * atan2(sqrt($a), sqrt(1-$a));
 				
 				return $R * $c;
-		
 			}
 
 			private function getNearestServer($serverlist, $latlong){
@@ -74,24 +80,13 @@
 			 * Get HTTP info
 			 */
 			private function getHTTPInfo($url){
-				$ch = curl_init(); 
-				curl_setopt($ch, CURLOPT_URL, $url); 
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE); 
-				curl_setopt($ch, CURLOPT_INTERFACE, "192.168.1.45");
-				curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36');
-				$raw = curl_exec($ch); 
-				if (curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200){
-					return false;
-				}
-				$info = curl_getinfo($ch);
-				return $info;
+				return $this->getHTML($url, true);
 			}
 
 			/**
 			 * Makes a simple GET request to a given URL. 
 			 */
-			private function getHTML($url){
+			private function getHTML($url, $return_info = false){
 				$ch = curl_init(); 
 				curl_setopt($ch, CURLOPT_URL, $url); 
 				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -103,6 +98,10 @@
 				if (curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200){
 					return false;
 				}
+				$info = curl_getinfo($ch);
+				curl_close($ch);
+				if ($return_info) return $info;
+				
 				return $raw;
 			}
 
@@ -129,8 +128,8 @@
 				if ($client = $configxml->getElementsByTagName('client')[0]){
 					$config['client'] = array(
 						'ip' => $client->getAttribute('ip'),
-						'lat' => $client->getAttribute('lat'),
-						'lon' => $client->getAttribute('lon'),
+						'lat' => floatval($client->getAttribute('lat')),
+						'lon' => floatval($client->getAttribute('lon')),
 						'isp' => $client->getAttribute('isp'),
 						'isprating' => $client->getAttribute('isprating'),
 						'country' => $client->getAttribute('country'),
@@ -141,7 +140,6 @@
 				}
 
 				return $config; 
-
 			}
 			
 			/**
@@ -156,8 +154,8 @@
 				foreach ($servers as $server){
 					$this_server = array(
 						'url' => $server->getAttribute("url"),
-						'lat' => $server->getAttribute("lat"),
-						'lon' => $server->getAttribute("lon"),
+						'lat' => floatval($server->getAttribute("lat")),
+						'lon' => floatval($server->getAttribute("lon")),
 						'name' => $server->getAttribute("name"),
 						'country' => $server->getAttribute("country"),
 						'cc' => $server->getAttribute("cc"),
